396c0f5c6206f56ef11acf306e6beaed37004518
Jacob Hernandez <jacobhdezm@outlook.com>
2022-03-11 14:56:27 -0600

    Version 0.4
    List of changes
    - Added comp_method_d and convol_method functions
    - Minor changes in documentation

--- R/gens.R
+++ R/gens.R
@@ -43,7 +43,7 @@ wh <- function(n, seed, a, c, m) {
 #' @param g sampling density function
 #' @param rg random number generator function with distribution g
 #' @param c constant or a vector containing the end-points of the interval to be searched
-#' for the maximum constant such that \eqn{f(y)/g(y)<=c} for all y.
+#' for the maximum constant such that \eqn{f(y)/g(y)<=c} for all \eqn{y}
 #' @export
 
 ar_method <- function(n, f, g, rg, c) {
@@ -66,11 +66,13 @@ ar_method <- function(n, f, g, rg, c) {
     return(r)
 }
 
-#' Composition Method
+#' Composition method
+#'
+#' \deqn{F(x) = \sum (p_i * F_i(x)) , 1 \le i \le k}
 #'
 #' @param n number of random numbers
-#' @param qfns list of the quantile functions in order
-#' @param p vector of probabilities in order
+#' @param qfns list of the \eqn{k} quantile functions in order
+#' @param p vector of \eqn{k} probabilities in order
 #' @export
 
 comp_method <- function(n, qfns, p){
@@ -93,6 +95,8 @@ comp_method <- function(n, qfns, p){
 
 #' Composition method (Continuous)
 #'
+#' \deqn{F(x) = \int_{-\infty}^{\infty} F_{X|Y=y}(x,y) f_{Y}(y) dy}
+#'
 #' @param n number of random numbers
 #' @param qfy quantile function of Y
 #' @param qfx quantile function of X|Y, where the first argument is the probability and the second
@@ -108,6 +112,56 @@ comp_method_c <- function(n, qfy, qfx) {
         u <- runif(2)
         r[i] <- qfx(u[2], qfy(u[1]))
 
+    }
+
+    return(r)
+}
+
+#' Composition method (Discrete)
+#'
+#' \deqn{Pr(X = x) = a * p_{x} + (1-a) * q_{x}}
+#'
+#' @param n number of random numbers
+#' @param a double constant
+#' @param qpx quantile function of px
+#' @param qqx quantile function of qx
+#' @export
+
+comp_method_d <- function(n, a, qpx, qqx) {
+
+    r <- numeric(n)
+
+    for (i in seq_len(n)) {
+
+        u <- runif(2)
+
+        if (u[1] < a) {
+            r[i] <- qpx(u[2])
+        } else {
+            r[i] <- qqx(u[2])
+        }
+    }
+
+    return(r)
+}
+
+#' Convolution method
+#'
+#' \deqn{Y ~ \sum (\beta_i * X_i) , 1 \le i \le k}
+#'
+#' @param n number of random numbers
+#' @param y function of the definition of y in terms of \eqn{x_i}
+#' @param qfns list of the \eqn{k} quantile functions in order
+#' @export
+
+convol_method <- function(n, y, qfns) {
+
+    k <- length(qfns)
+
+    r <- numeric(n)
+
+    for (i in seq_len(n)) {
+        r[i] <- purrr::map2_dbl(qfns, runif(k), function(qfn, u) qfn(u)) %>% y()
     }
 
     return(r)
